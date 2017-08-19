a6a36beab2dafd478146a8e72877a1b5003ddf03
Free Ekanayaka <free.ekanayaka@canonical.com>
2017-08-19 19:45:17 +0000

    Make go-command a local buffer variable in .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,13 +1,17 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
-(
- (go-mode
+((go-mode
   . ((go-test-args . "-tags libsqlite3")
      (eval
-      . (let* ((locals-path (let ((d (dir-locals-find-file "."))) (if (stringp d) (file-name-directory d) (car d))))
+      . (let* ((locals-path
+		(let ((d (dir-locals-find-file ".")))
+		  (if (stringp d) (file-name-directory d) (car d))))
 	       (sqlite-path (concat locals-path ".sqlite/")))
- 	  (progn
- 	    (setenv "CGO_CFLAGS" (concat "-I" sqlite-path))
- 	    (setenv "CGO_LDFLAGS" (concat "-L" sqlite-path))
- 	    (setenv "LD_LIBRARY_PATH" sqlite-path))))
-     )))
+	  (set
+	   (make-local-variable 'go-command)
+	   (s-concat
+	    "env "
+	    "CGO_CFLAGS=-I" sqlite-path " "
+	    "CGO_LDFLAGS=-L" sqlite-path " "
+	    "LD_LIBRARY_PATH=" sqlite-path " "
+	    "go")))))))
