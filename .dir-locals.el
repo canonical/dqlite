dab5bf2497d7d90828af5d8c849c315b6ff4c919
Free Ekanayaka <free.ekanayaka@canonical.com>
2017-09-12 12:23:20 +0000

    Don't set global env variable in .dir-locals.el.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,17 +1,16 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 ((go-mode
-  . ((go-test-args . "-tags libsqlite3")
+  . ((go-test-args . "-tags libsqlite3 -timeout 2s")
+     (eval
+      . (set
+	 (make-local-variable 'flycheck-go-build-tags)
+	 '("libsqlite3")))
      (eval
       . (let* ((locals-path
 		(let ((d (dir-locals-find-file ".")))
 		  (if (stringp d) (file-name-directory d) (car d))))
-	       (sqlite-path (concat locals-path ".sqlite/")))
-	  (set
-	   (make-local-variable 'go-command)
-	   (s-concat
-	    "env "
-	    "CGO_CFLAGS=-I" sqlite-path " "
-	    "CGO_LDFLAGS=-L" sqlite-path " "
-	    "LD_LIBRARY_PATH=" sqlite-path " "
-	    "go")))))))
+	       (go-wrapper (s-concat locals-path ".go-wrapper")))
+     	  (progn
+	    (set (make-local-variable 'go-command) go-wrapper)
+	    (set (make-local-variable 'flycheck-go-build-executable) go-wrapper)))))))
