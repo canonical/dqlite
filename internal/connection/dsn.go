1e06a867db4c52917843a04ac0f2f87058e6edda
Free Ekanayaka <free.ekanayaka@canonical.com>
2018-01-26 13:57:03 +0000

    Drop unused attributes of connection.Params

--- internal/connection/dsn.go
+++ internal/connection/dsn.go
@@ -3,9 +3,7 @@ package connection
 import (
 	"fmt"
 	"net/url"
-	"strconv"
 	"strings"
-	"time"
 )
 
 // Params captures details of dqlite leader connection parameters. Only pure file
@@ -14,10 +12,6 @@ import (
 type Params struct {
 	Filename string // Main database filename
 	Query    string // Opaque query string to pass down to go-sqlite3/SQLite
-
-	// Special dqlite parameters
-	LeadershipTimeout time.Duration // Maximum time to wait for leadership
-	InitializeTimeout time.Duration // Maximum time to wait for pending logs to be applied
 }
 
 // NewDSN parses the given sqlite3 DSN name checking if it's
@@ -25,9 +19,6 @@ type Params struct {
 func NewDSN(name string) (*Params, error) {
 	filename := name
 	query := ""
-
-	leadershipTimeout := 10000
-	initializeTimeout := 30000
 
 	pos := strings.IndexRune(name, '?')
 	if pos >= 1 {
@@ -37,20 +28,6 @@ func NewDSN(name string) (*Params, error) {
 		}
 		if params.Get("mode") == "memory" {
 			return nil, fmt.Errorf("can't replicate a memory database")
-		}
-		if key := params.Get("_leadership_timeout"); key != "" {
-			leadershipTimeout, err = strconv.Atoi(key)
-			if err != nil {
-				return nil, fmt.Errorf("leadership timeout is not a number: '%s'", key)
-			}
-			params.Del("_leadership_timeout")
-		}
-		if key := params.Get("_initialize_timeout"); key != "" {
-			initializeTimeout, err = strconv.Atoi(key)
-			if err != nil {
-				return nil, fmt.Errorf("initialize timeout is not a number: '%s'", key)
-			}
-			params.Del("_initialize_timeout")
 		}
 		query = params.Encode()
 		filename = filename[:pos]
@@ -69,16 +46,13 @@ func NewDSN(name string) (*Params, error) {
 	}
 
 	dsn := &Params{
-		Filename:          filename,
-		Query:             query,
-		LeadershipTimeout: time.Duration(leadershipTimeout) * time.Millisecond,
-		InitializeTimeout: time.Duration(initializeTimeout) * time.Millisecond,
+		Filename: filename,
+		Query:    query,
 	}
 	return dsn, nil
 }
 
-// Encode returns the full URI, including filename and query, but excluding any
-// dqlite-specific parameters.
+// Encode returns the full URI, including filename and query.
 func (d *Params) Encode() string {
 	query := ""
 	if d.Query != "" {
