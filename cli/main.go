4c6322a9ffdb5f59f9c6f0272ef5e07451091b43
Free Ekanayaka <free.ekanayaka@canonical.com>
2018-05-01 13:21:53 +0000

    Rename sub-package cli/ to cmd/

--- cli/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/CanonicalLtd/dqlite/cli/cmd"
-)
-
-func main() {
-	cmd.Execute()
-}
--- cmd/dqlite/root.go
+++ cmd/dqlite/root.go
@@ -1,20 +1,10 @@
-package cmd
+package main
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/spf13/cobra"
 )
-
-// Execute is the main entry point for the dqlite command line.
-func Execute() {
-	root := newRoot()
-	if err := root.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-}
 
 // Return a new root command.
 func newRoot() *cobra.Command {
@@ -24,8 +14,8 @@ func newRoot() *cobra.Command {
 		Long: `Replicate a SQLite database across a cluster, using the Raft algorithm.
 
 Complete documentation is available at https://github.com/CanonicalLtd/dqlite`,
-		Run: func(cmd *cobra.Command, args []string) {
-			// Do Stuff Here
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return fmt.Errorf("not implemented")
 		},
 	}
 	root.AddCommand(newDump())
