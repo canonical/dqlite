1ad90408dd71af2f0b04e3723071c376ccbba68c
Free Ekanayaka <free.ekanayaka@canonical.com>
2018-06-22 10:23:16 +0000

    Flush the log file when done

--- c/src/log.h
+++ c/src/log.h
@@ -4,15 +4,15 @@
 #include <stdio.h>
 #include <libgen.h>
 
-#define dqlite__errorf(P, MSG, CTX, ARGS...) \
-  fprintf(P->log, "ERRO: %10s (%40s:%4d): %-25s " CTX "\n", basename(__FILE__), __func__, __LINE__, MSG, ##ARGS)
+#define dqlite__errorf(P, MSG, CTX, ARGS...)				\
+	fprintf(P->log, "ERRO: %10s (%40s:%4d): %-25s " CTX "\n", basename(__FILE__), __func__, __LINE__, MSG, ##ARGS)
 
-#define dqlite__infof(P, MSG, CTX, ARGS...) \
-  fprintf(P->log, "INFO: %10s (%40s:%4d): %-25s " CTX "\n", basename(__FILE__), __func__, __LINE__, MSG, ##ARGS)
+#define dqlite__infof(P, MSG, CTX, ARGS...)				\
+	fprintf(P->log, "INFO: %10s (%40s:%4d): %-25s " CTX "\n", basename(__FILE__), __func__, __LINE__, MSG, ##ARGS)
 
 #ifdef DQLITE_DEBUG
-#define dqlite__debugf(P, MSG, CTX, ARGS...) \
-  fprintf(P->log, "DEBG: %10s (%40s:%4d): %-25s " CTX "\n", basename(__FILE__), __func__, __LINE__, MSG, ##ARGS)
+#define dqlite__debugf(P, MSG, CTX, ARGS...)				\
+	fprintf(P->log, "DEBG: %10s (%40s:%4d): %-25s " CTX "\n", basename(__FILE__), __func__, __LINE__, MSG, ##ARGS)
 #else
 #define dqlite__debugf(D, FORMAT, ARG...)
 #endif /* DQLITE_DEBUG */
