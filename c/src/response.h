4d8f4d7a9e06b07d448ce8b27c839be4977bd33c
Free Ekanayaka <free.ekanayaka@canonical.com>
2018-06-24 10:32:08 +0000

    Add DQLITE_RESPONSE_FAILURE response type

--- c/src/response.h
+++ c/src/response.h
@@ -7,6 +7,10 @@
  * a single IP packet, given typical MTU sizes */
 #define DQLITE__RESPONSE_BUF_SIZE 1024
 
+#define DQLITE__RESPONSE_SCHEMA_FAILURE(X, ...)	\
+	X(uint64, code,       __VA_ARGS__)	\
+	X(text,   description, __VA_ARGS__)
+
 #define DQLITE__RESPONSE_SCHEMA_SERVER(X, ...)		\
 	X(text, address, __VA_ARGS__)
 
@@ -39,6 +43,7 @@
 #define DQLITE__RESPONSE_SCHEMA_EMPTY(X, ...)	\
 	X(uint64, __unused__, __VA_ARGS__)
 
+DQLITE__SCHEMA_DEFINE(dqlite__response_failure,  DQLITE__RESPONSE_SCHEMA_FAILURE);
 DQLITE__SCHEMA_DEFINE(dqlite__response_server,   DQLITE__RESPONSE_SCHEMA_SERVER);
 DQLITE__SCHEMA_DEFINE(dqlite__response_welcome,  DQLITE__RESPONSE_SCHEMA_WELCOME);
 DQLITE__SCHEMA_DEFINE(dqlite__response_servers,  DQLITE__RESPONSE_SCHEMA_SERVERS);
@@ -50,6 +55,7 @@ DQLITE__SCHEMA_DEFINE(dqlite__response_rows,     DQLITE__RESPONSE_SCHEMA_ROWS);
 DQLITE__SCHEMA_DEFINE(dqlite__response_empty,    DQLITE__RESPONSE_SCHEMA_EMPTY);
 
 #define DQLITE__RESPONSE_SCHEMA_TYPES(X, ...)				\
+	X(DQLITE_RESPONSE_FAILURE,  dqlite__response_failure,  failure,  __VA_ARGS__) \
 	X(DQLITE_RESPONSE_SERVER,   dqlite__response_server,   server,   __VA_ARGS__) \
 	X(DQLITE_RESPONSE_WELCOME,  dqlite__response_welcome,  welcome,  __VA_ARGS__) \
 	X(DQLITE_RESPONSE_SERVERS,  dqlite__response_servers,  servers,  __VA_ARGS__) \
