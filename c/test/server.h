e18f89bb6368c6f6422c32f1f6d637314b253fc0
Free Ekanayaka <free.ekanayaka@canonical.com>
2018-07-27 14:41:48 +0000

    Cleanup test server

--- c/test/server.h
+++ c/test/server.h
@@ -3,11 +3,10 @@
 
 #include "client.h"
 
-typedef struct test_server test_server;
+struct test_server *test_server_start();
 
-test_server *test_server_start();
-int test_server_stop(test_server*);
+void test_server_stop(struct test_server *);
 
-int test_server_connect(test_server *t, struct test_client **client);
+void test_server_connect(struct test_server *t, struct test_client **client);
 
 #endif /* DQLITE_TEST_SERVER_H */
