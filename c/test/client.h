43e1a5073f492b94253470e54a899aa1016824ec
Free Ekanayaka <free.ekanayaka@canonical.com>
2018-07-27 12:18:55 +0000

    Cleanup test client

--- c/test/client.h
+++ c/test/client.h
@@ -1,35 +1,83 @@
+/******************************************************************************
+ *
+ * Test client speaking the dqlite wire protocol.
+ *
+ ******************************************************************************/
+
 #ifndef DQLITE_TEST_CLIENT_H
 #define DQLITE_TEST_CLIENT_H
 
 #include <stdint.h>
 
+#include <uv.h>
+
+#include "../src/request.h"
+#include "../src/response.h"
+
 struct test_client {
-	int fd;
+	int                     fd;
+	struct dqlite__request  request;
+	struct dqlite__response response;
+	uv_buf_t                bufs[3];
 };
 
-/*
- * Initialize a test client.
+struct test_client_result {
+	uint64_t last_insert_id;
+	uint64_t rows_affected;
+};
+
+struct test_client_row {
+	uint8_t *               types;
+	void **                 values;
+	struct test_client_row *next;
+};
+
+struct test_client_rows {
+	uint64_t                column_count;
+	const char **           column_names;
+	struct test_client_row *next;
+};
+
+/* Initialize a test client.
  *
  * @fd: The file descriptor for writing requests and reading responses.
  */
 void test_client_init(struct test_client *c, int fd);
 
-/*
- * Deallocate the memory used by the test client, if any.
- */
+/* Deallocate the memory used by the test client, if any. */
 void test_client_close(struct test_client *c);
 
-/*
- * Initialize the client, writing the protocol version.
- */
-int test_client_handshake(struct test_client* c);
+/* Initialize the connection by writing the protocol version. */
+void test_client_handshake(struct test_client *c);
 
-int test_client_leader(struct test_client *c, char **leader);
-int test_client_client(struct test_client *c, uint64_t *heartbeat);
-int test_client_open(struct test_client *c, const char *name, uint32_t *db_id);
-int test_client_prepare(struct test_client *c, uint32_t db_id, const char *sql, uint32_t *stmt_id);
-int test_client_exec(struct test_client *c, uint32_t db_id, uint32_t stmt_id);
-int test_client_query(struct test_client *c, uint32_t db_id, uint32_t stmt_id);
-int test_client_finalize(struct test_client *c, uint32_t db_id, uint32_t stmt_id);
+/* Perform a leader request */
+void test_client_leader(struct test_client *c, char **leader);
+
+/* Perform a client request */
+void test_client_client(struct test_client *c, uint64_t *heartbeat);
+
+/* Open a database */
+void test_client_open(struct test_client *c, const char *name, uint32_t *db_id);
+
+/* Prepare a statement */
+void test_client_prepare(struct test_client *c,
+                         uint32_t            db_id,
+                         const char *        sql,
+                         uint32_t *          stmt_id);
+
+/* Execute a prepared statement */
+void test_client_exec(struct test_client *       c,
+                      uint32_t                   db_id,
+                      uint32_t                   stmt_id,
+                      struct test_client_result *result);
+
+/* Fetch the result of a prepared statement */
+void test_client_query(struct test_client *     c,
+                       uint32_t                 db_id,
+                       uint32_t                 stmt_id,
+                       struct test_client_rows *rows);
+
+/* Destroy a prepared statement */
+void test_client_finalize(struct test_client *c, uint32_t db_id, uint32_t stmt_id);
 
 #endif /* DQLITE_TEST_CLIENT_H */
--- c/test/test_integration.c
+++ c/test/test_integration.c
@@ -64,70 +64,63 @@ static void tear_down(void *data) {
  *
  ******************************************************************************/
 
-static MunitResult test_dqlite(const MunitParameter params[], void *data) {
-	struct fixture *f = data;
-	int             err;
-	char *          leader;
-	uint64_t        heartbeat;
-	uint32_t        db_id;
-	uint32_t        stmt_id;
+static MunitResult test_exec_and_query(const MunitParameter params[], void *data) {
+	struct fixture *          f = data;
+	char *                    leader;
+	uint64_t                  heartbeat;
+	uint32_t                  db_id;
+	uint32_t                  stmt_id;
+	struct test_client_result result;
+	struct test_client_rows   rows;
 
 	(void)params;
 
-	err = test_client_handshake(f->client);
-	munit_assert_int(err, ==, 0);
-
-	err = test_client_leader(f->client, &leader);
-	munit_assert_int(err, ==, 0);
-
-	err = test_client_client(f->client, &heartbeat);
-	munit_assert_int(err, ==, 0);
-
-	err = test_client_open(f->client, "test.db", &db_id);
-	munit_assert_int(err, ==, 0);
-
+	/* Initialize the connection and open a database. */
+	test_client_handshake(f->client);
+	test_client_leader(f->client, &leader);
+	test_client_client(f->client, &heartbeat);
+	test_client_open(f->client, "test.db", &db_id);
 	munit_assert_int(db_id, ==, 0);
 
-	err = test_client_prepare(
-	    f->client, db_id, "CREATE TABLE test (n INT)", &stmt_id);
-	munit_assert_int(err, ==, 0);
+	/* Create a test table. */
+	test_client_prepare(f->client, db_id, "CREATE TABLE test (n INT)", &stmt_id);
+	test_client_exec(f->client, db_id, stmt_id, &result);
+	test_client_finalize(f->client, db_id, stmt_id);
 
-	munit_assert_int(stmt_id, ==, 0);
-
-	err = test_client_exec(f->client, db_id, stmt_id);
-	munit_assert_int(err, ==, 0);
-
-	err = test_client_finalize(f->client, db_id, stmt_id);
-	munit_assert_int(err, ==, 0);
-
-	err = test_client_prepare(
+	/* Insert a row in the test table. */
+	test_client_prepare(
 	    f->client, db_id, "INSERT INTO test VALUES(123)", &stmt_id);
-	munit_assert_int(err, ==, 0);
 
 	munit_assert_int(stmt_id, ==, 0);
 
-	err = test_client_exec(f->client, db_id, stmt_id);
-	munit_assert_int(err, ==, 0);
+	test_client_exec(f->client, db_id, stmt_id, &result);
 
-	err = test_client_finalize(f->client, db_id, stmt_id);
-	munit_assert_int(err, ==, 0);
+	munit_assert_int(result.last_insert_id, ==, 1);
+	munit_assert_int(result.rows_affected, ==, 1);
 
-	err = test_client_prepare(f->client, db_id, "SELECT n FROM test", &stmt_id);
-	munit_assert_int(err, ==, 0);
+	test_client_finalize(f->client, db_id, stmt_id);
+
+	/* Select rows from the test table. */
+	test_client_prepare(f->client, db_id, "SELECT n FROM test", &stmt_id);
 
 	munit_assert_int(stmt_id, ==, 0);
 
-	err = test_client_query(f->client, db_id, stmt_id);
-	munit_assert_int(err, ==, 0);
+	test_client_query(f->client, db_id, stmt_id, &rows);
 
-	err = test_client_finalize(f->client, db_id, stmt_id);
-	munit_assert_int(err, ==, 0);
+	munit_assert_int(rows.column_count, ==, 1);
+	munit_assert_string_equal(rows.column_names[0], "n");
+
+	munit_assert_ptr_not_null(rows.next);
+	munit_assert_int(rows.next->types[0], ==, SQLITE_INTEGER);
+	munit_assert_int(*(int64_t *)rows.next->values[0], ==, 123);
+
+	test_client_finalize(f->client, db_id, stmt_id);
 
 	return MUNIT_OK;
 }
 
 static MunitTest dqlite__integration_tests[] = {
-    {"/dqlite", test_dqlite, setup, tear_down, 0, NULL},
+    {"/exec-and-query", test_exec_and_query, setup, tear_down, 0, NULL},
     {NULL, NULL, NULL, NULL, 0, NULL},
 };
 
