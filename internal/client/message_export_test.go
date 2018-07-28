4c2de1b1e693fa708ac5b778d750689df9933fc2
Free Ekanayaka <free.ekanayaka@canonical.com>
2018-07-28 12:12:10 +0000

    Delete Go code

--- internal/client/message_export_test.go
+++
@@ -1,5 +0,0 @@
-package client
-
-func (m *Message) Body1() ([]byte, int) {
-	return m.body1.Bytes, m.body1.Offset
-}
--- internal/registry/conn_export_test.go
+++
@@ -1,5 +0,0 @@
-package registry
-
-func (r *Registry) ConnSerialReset() {
-	serial = 0
-}
