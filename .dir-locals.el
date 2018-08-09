022a7954f2ad0eff356b273d8251ae8204b33655
Free Ekanayaka <free.ekanayaka@canonical.com>
2018-08-09 19:57:22 +0200

    Add --enable-experimental flag

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ((nil . ((fill-column . 80)))
  (c-mode . ((c-file-style . "linux-tabs-only")
-	    (flycheck-gcc-definitions . ("DQLITE_DEBUG"))
+	    (flycheck-gcc-definitions . ("DQLITE_DEBUG" "DQLITE_EXPERIMENTAL"))
 	    (flycheck-gcc-include-path . ("../include")))))
