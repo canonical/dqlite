8ff12aa425a9f37b9e0ee0c661ab91fedf9df5fb
Free Ekanayaka <free.ekanayaka@canonical.com>
2018-08-09 20:19:51 +0200

    Add faulty sqlite3_mem_methods implementation
    This is basically a copy of SQLite's original version, included in the test
    builds but not exposed as public API.

---
+++ test/mem.h
@@ -0,0 +1,23 @@
+#ifndef DQLITE_TEST_MEM_H
+#define DQLITE_TEST_MEM_H
+
+#include <sqlite3.h>
+
+/* Get the current number of outstanding malloc()'s without a matching free()
+ * and the total number of used memory. */
+void test_mem_stats(int *malloc_count, int *memory_used);
+
+/* Wrap the given SQLite memory management instance with the faulty memory
+ * management interface. By default no faults will be triggered. */
+void test_mem_fault_wrap(sqlite3_mem_methods *m, sqlite3_mem_methods *wrap);
+
+/* Unwrap the given faulty memory management instance returning the original
+ * one. */
+void test_mem_fault_unwrap(sqlite3_mem_methods *wrap, sqlite3_mem_methods *m);
+
+/* Configure the faulty memory management implementation so malloc()-related
+ * functions start returning NULL pointers after 'delay' calls, and keep failing
+ * for 'repeat' consecutive times. */
+void test_mem_fault_config(int delay, int repeat);
+
+#endif /* DQLITE_TEST_MEM_H */
