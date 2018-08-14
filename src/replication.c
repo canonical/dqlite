c7a20ccc05eb1711a9c7a85e154c4d1f08cb0d49
Free Ekanayaka <free.ekanayaka@canonical.com>
2018-08-14 17:50:55 +0200

    Initial test exercising concurrent writes from different coroutines

--- src/replication.c
+++ src/replication.c
@@ -3,31 +3,36 @@
 #include <assert.h>
 #include <stdlib.h>
 
+#include <libco.h>
 #include <sqlite3.h>
 
 #include "lifecycle.h"
 #include "replication.h"
 
-void dqlite__replication_ctx_init(struct dqlite__replication_ctx *c) {
+void dqlite__replication_ctx_init(struct dqlite__replication_ctx *c)
+{
 	assert(c != NULL);
 
 	dqlite__lifecycle_init(DQLITE__LIFECYCLE_REPLICATION);
 };
 
-void dqlite__replication_ctx_close(struct dqlite__replication_ctx *c) {
+void dqlite__replication_ctx_close(struct dqlite__replication_ctx *c)
+{
 	assert(c != NULL);
 
 	dqlite__lifecycle_close(DQLITE__LIFECYCLE_REPLICATION);
 }
 
-int dqlite__replication_begin(sqlite3_wal_replication *r, void *arg) {
+int dqlite__replication_begin(sqlite3_wal_replication *r, void *arg)
+{
 	(void)r;
 	(void)arg;
 
 	return SQLITE_OK;
 }
 
-int dqlite__replication_abort(sqlite3_wal_replication *r, void *arg) {
+int dqlite__replication_abort(sqlite3_wal_replication *r, void *arg)
+{
 	(void)r;
 	(void)arg;
 
@@ -40,7 +45,10 @@ int dqlite__replication_frames(sqlite3_wal_replication *      r,
                                int                            n,
                                sqlite3_wal_replication_frame *frames,
                                unsigned                       truncate,
-                               int                            commit) {
+                               int                            commit)
+{
+	struct dqlite__replication_ctx *ctx;
+
 	(void)arg;
 	(void)page_size;
 	(void)n;
@@ -51,17 +59,23 @@ int dqlite__replication_frames(sqlite3_wal_replication *      r,
 	assert(r != NULL);
 	assert(r->pAppData != NULL);
 
+	ctx = r->pAppData;
+
+	co_switch(ctx->main_coroutine);
+
 	return SQLITE_OK;
 }
 
-int dqlite__replication_undo(sqlite3_wal_replication *r, void *arg) {
+int dqlite__replication_undo(sqlite3_wal_replication *r, void *arg)
+{
 	(void)r;
 	(void)arg;
 
 	return SQLITE_OK;
 }
 
-int dqlite__replication_end(sqlite3_wal_replication *r, void *arg) {
+int dqlite__replication_end(sqlite3_wal_replication *r, void *arg)
+{
 	(void)r;
 	(void)arg;
 
