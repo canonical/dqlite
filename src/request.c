515e6f7afc7d8d4c780ca8521c0739f1ade34755
Free Ekanayaka <free.ekanayaka@canonical.com>
2018-08-16 17:09:55 +0200

    Implement interrupt requests
    The request type DQLITE_REQUEST_INTERRUPT can be now used to cancel ongoing
    query requests.

--- src/request.c
+++ src/request.c
@@ -17,12 +17,17 @@ DQLITE__SCHEMA_IMPLEMENT(dqlite__request_client, DQLITE__REQUEST_SCHEMA_CLIENT);
 DQLITE__SCHEMA_IMPLEMENT(dqlite__request_heartbeat,
                          DQLITE__REQUEST_SCHEMA_HEARTBEAT);
 DQLITE__SCHEMA_IMPLEMENT(dqlite__request_open, DQLITE__REQUEST_SCHEMA_OPEN);
-DQLITE__SCHEMA_IMPLEMENT(dqlite__request_prepare, DQLITE__REQUEST_SCHEMA_PREPARE);
+DQLITE__SCHEMA_IMPLEMENT(dqlite__request_prepare,
+                         DQLITE__REQUEST_SCHEMA_PREPARE);
 DQLITE__SCHEMA_IMPLEMENT(dqlite__request_exec, DQLITE__REQUEST_SCHEMA_EXEC);
 DQLITE__SCHEMA_IMPLEMENT(dqlite__request_query, DQLITE__REQUEST_SCHEMA_QUERY);
-DQLITE__SCHEMA_IMPLEMENT(dqlite__request_finalize, DQLITE__REQUEST_SCHEMA_FINALIZE);
-DQLITE__SCHEMA_IMPLEMENT(dqlite__request_exec_sql, DQLITE__REQUEST_SCHEMA_EXEC_SQL);
+DQLITE__SCHEMA_IMPLEMENT(dqlite__request_finalize,
+                         DQLITE__REQUEST_SCHEMA_FINALIZE);
+DQLITE__SCHEMA_IMPLEMENT(dqlite__request_exec_sql,
+                         DQLITE__REQUEST_SCHEMA_EXEC_SQL);
 DQLITE__SCHEMA_IMPLEMENT(dqlite__request_query_sql,
                          DQLITE__REQUEST_SCHEMA_QUERY_SQL);
+DQLITE__SCHEMA_IMPLEMENT(dqlite__request_interrupt,
+                         DQLITE__REQUEST_SCHEMA_INTERRUPT);
 
 DQLITE__SCHEMA_HANDLER_IMPLEMENT(dqlite__request, DQLITE__REQUEST_SCHEMA_TYPES);
