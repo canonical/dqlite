2dbe79226f60344f77581e41fc79d95337a425b7
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-03-07 17:19:19 +0100

    Use test macros in test_server

--- test/test_server.c
+++ test/test_server.c
@@ -6,16 +6,21 @@
 
 #include "client.h"
 #include "cluster.h"
-#include "leak.h"
 #include "log.h"
-#include "munit.h"
 #include "server.h"
+#include "./lib/runner.h"
+#include "./lib/sqlite.h"
+#include "./lib/heap.h"
+
+TEST_MODULE(server);
 
 /******************************************************************************
  *
  * Setup and tear down
  *
  ******************************************************************************/
+
+dqlite_cluster *cluster;
 
 static void *setup(const MunitParameter params[], void *user_data) {
 	dqlite_server *server;
@@ -24,7 +29,12 @@ static void *setup(const MunitParameter params[], void *user_data) {
 	(void)params;
 	(void)user_data;
 
-	err = dqlite_server_create(test_cluster(), &server);
+	test_heap_setup(params, user_data);
+	test_sqlite_setup(params);
+
+	cluster = test_cluster();
+
+	err = dqlite_server_create(cluster, &server);
 	munit_assert_int(err, ==, 0);
 
 	return server;
@@ -34,8 +44,10 @@ static void tear_down(void *data) {
 	dqlite_server *server = data;
 
 	dqlite_server_destroy(server);
+	test_cluster_close(cluster);
 
-	test_assert_no_leaks();
+	test_sqlite_tear_down();
+	test_heap_tear_down(data);
 }
 
 /******************************************************************************
@@ -44,7 +56,12 @@ static void tear_down(void *data) {
  *
  ******************************************************************************/
 
-static MunitResult test_config_logger(const MunitParameter params[], void *data) {
+TEST_SUITE(config);
+TEST_SETUP(config, setup);
+TEST_TEAR_DOWN(config, tear_down);
+
+TEST_CASE(config, logger, NULL)
+{
 	dqlite_server *server = data;
 	dqlite_logger *logger = test_logger();
 	int            err;
@@ -56,11 +73,13 @@ static MunitResult test_config_logger(const MunitParameter params[], void *data)
 
 	munit_assert_ptr_equal(dqlite_server_logger(server), logger);
 
+	free(logger);
+
 	return MUNIT_OK;
 }
 
-static MunitResult test_config_heartbeat_timeout(const MunitParameter params[],
-                                                 void *               data) {
+TEST_CASE(config, heartbeat_timeout, NULL)
+{
 	dqlite_server *server  = data;
 	int            timeout = 1000;
 	int            err;
@@ -74,7 +93,8 @@ static MunitResult test_config_heartbeat_timeout(const MunitParameter params[],
 	return MUNIT_OK;
 }
 
-static MunitResult test_config_page_size(const MunitParameter params[], void *data) {
+TEST_CASE(config, page_size, NULL)
+{
 	dqlite_server *server = data;
 	int            size   = 512;
 	int            err;
@@ -87,8 +107,8 @@ static MunitResult test_config_page_size(const MunitParameter params[], void *da
 	return MUNIT_OK;
 }
 
-static MunitResult test_config_checkpoint_threshold(const MunitParameter params[],
-                                                    void *               data) {
+TEST_CASE(config, checkpoint_threshold, NULL)
+{
 	dqlite_server *server    = data;
 	int            threshold = 1;
 	int            err;
@@ -101,27 +121,3 @@ static MunitResult test_config_checkpoint_threshold(const MunitParameter params[
 
 	return MUNIT_OK;
 }
-
-static MunitTest dqlite_server_config_tests[] = {
-    {"/logger", test_config_logger, setup, tear_down, 0, NULL},
-    {"/heartbeat-timeout", test_config_heartbeat_timeout, setup, tear_down, 0, NULL},
-    {"/page-size", test_config_page_size, setup, tear_down, 0, NULL},
-    {"/checkpoint-threshold",
-     test_config_checkpoint_threshold,
-     setup,
-     tear_down,
-     0,
-     NULL},
-    {NULL, NULL, NULL, NULL, 0, NULL},
-};
-
-/******************************************************************************
- *
- * Suite
- *
- ******************************************************************************/
-
-MunitSuite dqlite__server_suites[] = {
-    {"_config", dqlite_server_config_tests, NULL, 1, MUNIT_SUITE_OPTION_NONE},
-    {NULL, NULL, NULL, 0, 0},
-};
