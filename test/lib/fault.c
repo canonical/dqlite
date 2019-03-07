363c3f8518037596d9e99c03fdd55f24f7302bdf
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-03-07 08:19:56 +0100

    Factor out mem fault implementation

---
+++ test/lib/fault.c
@@ -0,0 +1,63 @@
+#include "fault.h"
+#include "munit.h"
+
+void test_fault_init(struct test_fault *f)
+{
+    f->countdown = -1;
+    f->n = -1;
+    f->enabled = false;
+}
+
+bool test_fault_tick(struct test_fault *f)
+{
+    if (MUNIT_UNLIKELY(!f->enabled)) {
+        return false;
+    }
+
+    /* If the initial delay parameter was set to -1, then never fail. This is
+     * the most common case. */
+    if (MUNIT_LIKELY(f->countdown < 0)) {
+        return false;
+    }
+
+    /* If we did not yet reach 'delay' ticks, then just decrease the countdown.
+     */
+    if (f->countdown > 0) {
+        f->countdown--;
+        return false;
+    }
+
+    munit_assert_int(f->countdown, ==, 0);
+
+    /* We reached 'delay' ticks, let's see how many times we have to trigger the
+     * fault, if any. */
+
+    if (f->n < 0) {
+        /* Trigger the fault forever. */
+        return true;
+    }
+
+    if (f->n > 0) {
+        /* Trigger the fault at least this time. */
+        f->n--;
+        return true;
+    }
+
+    munit_assert_int(f->n, ==, 0);
+
+    /* We reached 'repeat' ticks, let's stop triggering the fault. */
+    f->countdown--;
+
+    return false;
+}
+
+void test_fault_config(struct test_fault *f, int delay, int repeat)
+{
+    f->countdown = delay;
+    f->n = repeat;
+}
+
+void test_fault_enable(struct test_fault *f)
+{
+    f->enabled = true;
+}
---
+++ test/lib/fault.h
@@ -0,0 +1,41 @@
+/**
+ * Helper for test components supporting fault injection.
+ */
+
+#ifndef TEST_FAULT_H
+#define TEST_FAULT_H
+
+#include <stdbool.h>
+
+/**
+ * Information about a fault that should occurr in a component.
+ */
+struct test_fault
+{
+    int countdown; /* Trigger the fault when this counter gets to zero. */
+    int n;         /* Repeat the fault this many times. Default is -1. */
+    bool enabled;   /* Enable fault triggering. */
+};
+
+/**
+ * Initialize a fault.
+ */
+void test_fault_init(struct test_fault *f);
+
+/**
+ * Advance the counters of the fault. Return true if the fault should be
+ * triggered, false otherwise.
+ */
+bool test_fault_tick(struct test_fault *f);
+
+/**
+ * Configure the fault with the given values.
+ */
+void test_fault_config(struct test_fault *f, int delay, int repeat);
+
+/**
+ * Enable fault triggering.
+ */
+void test_fault_enable(struct test_fault *f);
+
+#endif /* TEST_FAULT_H */
