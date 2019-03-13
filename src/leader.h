e5b03aef89f70e6bc2988df9d6ec65d09f32c87f
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-03-13 15:14:44 +0100

    Add leader exec interface

--- src/leader.h
+++ src/leader.h
@@ -14,8 +14,24 @@ struct leader
 	queue queue;
 };
 
+struct exec;
+typedef void (*exec_cb)(struct exec *req, int status);
+
+struct exec
+{
+	void *data;
+	struct leader *leader;
+	sqlite3_stmt *stmt;
+	queue queue;
+};
+
 int leader__init(struct leader *l, struct db *db);
 
 void leader__close(struct leader *l);
+
+int leader__exec(struct leader *l,
+		 struct exec *req,
+		 sqlite3_stmt *stmt,
+		 exec_cb cb);
 
 #endif /* LEADER_H_*/
