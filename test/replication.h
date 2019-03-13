030f237f97eb4856616f2b604b35a98988e8e018
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-03-13 17:36:11 +0100

    Use stub replication in gateway tests

--- test/replication.h
+++ test/replication.h
@@ -3,6 +3,15 @@
 
 #include <sqlite3.h>
 
-sqlite3_wal_replication* test_replication();
+sqlite3_wal_replication *test_replication();
+
+#define FIXTURE_STUB_REPLICATION sqlite3_wal_replication replication;
+
+#define SETUP_STUB_REPLICATION                \
+	f->replication = *test_replication(); \
+	sqlite3_wal_replication_register(&f->replication, 0)
+
+#define TEAR_DOWN_STUB_REPLICATION \
+	sqlite3_wal_replication_unregister(&f->replication);
 
 #endif /* DQLITE_TEST_REPLICATION_H */
