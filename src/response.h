88b9634eb5f9a1aea24b8d8175c63ec2e5cd29b9
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-03-31 23:37:06 +0200

    Implement open/db gateway handler

--- src/response.h
+++ src/response.h
@@ -11,13 +11,17 @@
 
 #define RESPONSE_SERVER(X, ...) X(text, address, ##__VA_ARGS__)
 #define RESPONSE_WELCOME(X, ...) X(uint64, heartbeat_timeout, ##__VA_ARGS__)
+#define RESPONSE_DB(X, ...)          \
+	X(uint32, id, ##__VA_ARGS__) \
+	X(uint32, __pad__, ##__VA_ARGS__)
 
 #define RESPONSE__DEFINE(LOWER, UPPER, _) \
 	SERIALIZE__DEFINE(response_##LOWER, RESPONSE_##UPPER);
 
-#define RESPONSE__TYPES(X, ...) \
-	X(server, SERVER, __VA_ARGS__) \
-	X(welcome, WELCOME, __VA_ARGS__)
+#define RESPONSE__TYPES(X, ...)          \
+	X(server, SERVER, __VA_ARGS__)   \
+	X(welcome, WELCOME, __VA_ARGS__) \
+	X(db, DB, __VA_ARGS__)
 
 RESPONSE__TYPES(RESPONSE__DEFINE);
 
@@ -57,7 +61,7 @@ SCHEMA__DEFINE(response_failure, RESPONSE__SCHEMA_FAILURE);
 SCHEMA__DEFINE(response_server_, RESPONSE__SCHEMA_SERVER);
 SCHEMA__DEFINE(response_welcome_, RESPONSE__SCHEMA_WELCOME);
 SCHEMA__DEFINE(response_servers, RESPONSE__SCHEMA_SERVERS);
-SCHEMA__DEFINE(response_db, RESPONSE__SCHEMA_DB);
+SCHEMA__DEFINE(response_db_, RESPONSE__SCHEMA_DB);
 SCHEMA__DEFINE(response_stmt, RESPONSE__SCHEMA_STMT);
 SCHEMA__DEFINE(response_result, RESPONSE__SCHEMA_RESULT);
 SCHEMA__DEFINE(response_rows, RESPONSE__SCHEMA_ROWS);
@@ -68,7 +72,7 @@ SCHEMA__DEFINE(response_empty, RESPONSE__SCHEMA_EMPTY);
 	X(DQLITE_RESPONSE_SERVER, response_server_, server, __VA_ARGS__)    \
 	X(DQLITE_RESPONSE_WELCOME, response_welcome_, welcome, __VA_ARGS__) \
 	X(DQLITE_RESPONSE_SERVERS, response_servers, servers, __VA_ARGS__)  \
-	X(DQLITE_RESPONSE_DB, response_db, db, __VA_ARGS__)                 \
+	X(DQLITE_RESPONSE_DB, response_db_, db, __VA_ARGS__)                \
 	X(DQLITE_RESPONSE_STMT, response_stmt, stmt, __VA_ARGS__)           \
 	X(DQLITE_RESPONSE_RESULT, response_result, result, __VA_ARGS__)     \
 	X(DQLITE_RESPONSE_ROWS, response_rows, rows, __VA_ARGS__)           \
