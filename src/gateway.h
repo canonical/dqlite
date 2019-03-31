2017122928d06a609689a4ad6abb8c318d363f6e
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-03-31 21:17:19 +0200

    Initial skeleton of gateway__handle

--- src/gateway.h
+++ src/gateway.h
@@ -9,9 +9,14 @@
 
 #include "../include/dqlite.h"
 
+#include "./lib/buffer.h"
+#include "./lib/serialize.h"
+
 #include "leader.h"
 #include "options.h"
 #include "registry.h"
+
+struct handle;
 
 /**
  * Handle requests from a single connected client and forward them to
@@ -23,7 +28,8 @@ struct gateway
 	struct options *options;      /* Configuration options */
 	struct registry *registry;    /* Register of existing databases */
 	struct raft *raft;	    /* Raft instance */
-	struct leader *leader;
+	struct leader *leader;	/* Leader connection to the database */
+	struct handle *req;	   /* Asynchronous request being handled */
 };
 
 void gateway__init(struct gateway *g,
@@ -33,5 +39,34 @@ void gateway__init(struct gateway *g,
 		   struct raft *raft);
 
 void gateway__close(struct gateway *g);
+
+/**
+ * Asynchronous request to handle a client command.
+ */
+typedef void (*handle_cb)(struct handle *req, int status);
+struct handle
+{
+	void *data; /* User data */
+	struct gateway *gateway;
+	struct buffer *buffer;
+	handle_cb cb;
+};
+
+/**
+ * Start handling a new client request.
+ *
+ * At most one request can be outstanding at any given time. This function will
+ * return an error if user code calls it and there's already a request in
+ * progress.
+ *
+ * The @type parameter holds the request type code (e.g. #REQUEST_LEADER), the
+ * @cursor parameter holds a cursor for reading the request payload, and the
+ * @buffer parameter is a buffer for writing the response.
+ */
+int gateway__handle(struct gateway *g,
+		    struct handle *req,
+		    int type,
+		    struct cursor *cursor,
+		    struct buffer *buffer);
 
 #endif /* DQLITE_GATEWAY_H_ */
