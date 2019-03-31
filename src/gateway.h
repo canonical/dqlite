5799f2066afc7c854b01b4133385791fbbe851d7
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-03-31 20:22:33 +0200

    Add raft field to struct gateway

--- src/gateway.h
+++ src/gateway.h
@@ -5,15 +5,13 @@
 #ifndef DQLITE_GATEWAY_H_
 #define DQLITE_GATEWAY_H_
 
+#include <raft.h>
+
 #include "../include/dqlite.h"
 
-#include "db_.h"
-#include "error.h"
 #include "leader.h"
 #include "options.h"
 #include "registry.h"
-#include "request.h"
-#include "response.h"
 
 /**
  * Handle requests from a single connected client and forward them to
@@ -23,14 +21,16 @@ struct gateway
 {
 	struct dqlite_logger *logger; /* Logger to use */
 	struct options *options;      /* Configuration options */
-	struct registry *registry;
+	struct registry *registry;    /* Register of existing databases */
+	struct raft *raft;	    /* Raft instance */
 	struct leader *leader;
 };
 
 void gateway__init(struct gateway *g,
 		   struct dqlite_logger *logger,
 		   struct options *options,
-		   struct registry *registry);
+		   struct registry *registry,
+		   struct raft *raft);
 
 void gateway__close(struct gateway *g);
 
