2ad36b89d20431f4b9045172930066c930dfe732
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-04-02 18:10:36 +0200

    Move query code into its own module

---
+++ src/query.h
@@ -0,0 +1,18 @@
+/**
+ * Step through a query progressively encoding a the row tuples.
+ */
+
+#ifndef QUERY_H_
+#define QUERY_H_
+
+#include "lib/serialize.h"
+#include "lib/buffer.h"
+
+/**
+ * Step through the given query statement progressively encoding the yielded row
+ * tuples, either until #SQLITE_DONE is returned or a full page of the given
+ * buffer is filled.
+ */
+int query__batch(sqlite3_stmt *stmt, struct buffer *buffer);
+
+#endif /* QUERY_H_*/
