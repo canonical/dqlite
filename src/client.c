c10f5a4b3231b887c156450d5c123d0fcdf7a937
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-04-03 15:32:58 +0200

    Add initial client core logic

--- src/client.c
+++ src/client.c
@@ -1,2 +1,28 @@
+#include <unistd.h>
+
 #include "client.h"
 #include "request.h"
+
+void client__init(struct client *c, int fd)
+{
+	c->fd = fd;
+}
+
+int client__handshake(struct client *c)
+{
+	uint64_t protocol;
+	int rv;
+
+	protocol = byte__flip64(DQLITE_PROTOCOL_VERSION);
+
+	rv = write(c->fd, &protocol, sizeof(protocol));
+	if (rv < 0) {
+		return DQLITE_ERROR;
+	}
+
+	return 0;
+}
+
+void client__close(struct client *c) {
+	close(c->fd);
+}
--- src/client.h
+++ src/client.h
@@ -1,9 +1,27 @@
 /**
- * Basic dqlite client logic for encoding requests and decoding responses.
+ * Core dqlite client logic for encoding requests and decoding responses.
  */
 
 #ifndef CLIENT_H_
 #define CLIENT_H_
 
+struct client {
+	int fd; /* Connected socket */
+};
+
+/**
+ * Initialize a new client, writing requests to fd.
+ */
+void client__init(struct client *c, int fd);
+
+/**
+ * Initialize the connection by writing the protocol version.
+ */
+int client__handshake(struct client *c);
+
+/**
+ * Release all memory used by the client, and close the client socket.
+ */
+void client__close(struct client *c);
 
 #endif /* CLIENT_H_*/
