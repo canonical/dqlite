aad9a3d2e14ed87f4161d723d84f2746e2400b09
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-04-05 13:00:51 +0200

    Implement dqlite_handle

--- test/unit/test_server.c
+++ test/unit/test_server.c
@@ -1,6 +1,7 @@
 #include "../lib/fs.h"
 #include "../lib/heap.h"
 #include "../lib/runner.h"
+#include "../lib/socket.h"
 #include "../lib/sqlite.h"
 #include "../lib/thread.h"
 
@@ -14,23 +15,23 @@ TEST_MODULE(server);
  *
  ******************************************************************************/
 
-#define FIXTURE \
-	FIXTURE_THREAD;\
-	char *dir;\
+#define FIXTURE         \
+	FIXTURE_THREAD; \
+	char *dir;      \
 	struct dqlite dqlite
 
-#define SETUP \
-	int rv;\
-	SETUP_HEAP;\
-	SETUP_SQLITE;\
-	f->dir = test_dir_setup();\
-	rv = dqlite__init(&f->dqlite, 1, "1", f->dir);\
+#define SETUP                                          \
+	int rv;                                        \
+	SETUP_HEAP;                                    \
+	SETUP_SQLITE;                                  \
+	f->dir = test_dir_setup();                     \
+	rv = dqlite__init(&f->dqlite, 1, "1", f->dir); \
 	munit_assert_int(rv, ==, 0)
 
-#define TEAR_DOWN \
-	dqlite__close(&f->dqlite);\
-	test_dir_tear_down(f->dir);\
-	TEAR_DOWN_SQLITE;\
+#define TEAR_DOWN                   \
+	dqlite__close(&f->dqlite);  \
+	test_dir_tear_down(f->dir); \
+	TEAR_DOWN_SQLITE;           \
 	TEAR_DOWN_HEAP
 
 /******************************************************************************
@@ -53,9 +54,21 @@ static void *run(void *arg)
 /* Run the dqlite server in a thread */
 #define START THREAD_START(run, &f->dqlite)
 
+/* Wait for the server to be ready */
 #define READY munit_assert_true(dqlite_ready(&f->dqlite))
 
-#define STOP dqlite_stop(&f->dqlite); THREAD_JOIN
+/* Stop the server and wait for it to be done */
+#define STOP                     \
+	dqlite_stop(&f->dqlite); \
+	THREAD_JOIN
+
+/* Handle a new connection */
+#define HANDLE(FD)                                   \
+	{                                            \
+		int rv2;                             \
+		rv2 = dqlite_handle(&f->dqlite, FD); \
+		munit_assert_int(rv2, ==, 0);        \
+	}
 
 /******************************************************************************
  *
@@ -63,17 +76,20 @@ static void *run(void *arg)
  *
  ******************************************************************************/
 
-struct run_fixture {
+struct run_fixture
+{
 	FIXTURE;
 };
 
 TEST_SUITE(run);
-TEST_SETUP(run) {
+TEST_SETUP(run)
+{
 	struct run_fixture *f = munit_malloc(sizeof *f);
 	SETUP;
 	return f;
 }
-TEST_TEAR_DOWN(run) {
+TEST_TEAR_DOWN(run)
+{
 	struct run_fixture *f = data;
 	TEAR_DOWN;
 	free(f);
@@ -89,114 +105,42 @@ TEST_CASE(run, success, NULL)
 	return MUNIT_OK;
 }
 
-#if 0
-
 /******************************************************************************
  *
- * Setup and tear down
+ * dqlite_handle
  *
  ******************************************************************************/
 
-dqlite_cluster *cluster;
-
-static void *setup(const MunitParameter params[], void *user_data) {
-	dqlite *server;
-	int            err;
-
-	(void)params;
-	(void)user_data;
-
-	test_heap_setup(params, user_data);
-	test_sqlite_setup(params);
-
-	cluster = test_cluster();
-
-	err = dqlite_create(cluster, &server);
-	munit_assert_int(err, ==, 0);
-
-	return server;
-}
-
-static void tear_down(void *data) {
-	dqlite *server = data;
-
-	dqlite_destroy(server);
-	test_cluster_close(cluster);
-
-	test_sqlite_tear_down();
-	test_heap_tear_down(data);
-}
-
-/******************************************************************************
- *
- * Tests dqlite_config
- *
- ******************************************************************************/
-
-TEST_SUITE(config);
-TEST_SETUP(config, setup);
-TEST_TEAR_DOWN(config, tear_down);
-
-TEST_CASE(config, logger, NULL)
+struct handle_fixture
 {
-	dqlite *server = data;
-	dqlite_logger *logger = test_logger();
-	int            err;
+	FIXTURE;
+	struct test_socket_pair sockets;
+};
 
+TEST_SUITE(handle);
+TEST_SETUP(handle)
+{
+	struct handle_fixture *f = munit_malloc(sizeof *f);
+	SETUP;
+	START;
+	READY;
+	test_socket_pair_setup(params, &f->sockets);
+	return f;
+}
+TEST_TEAR_DOWN(handle)
+{
+	struct handle_fixture *f = data;
+	test_socket_pair_tear_down(&f->sockets);
+	STOP;
+	TEAR_DOWN;
+	free(f);
+}
+
+TEST_CASE(handle, success, NULL)
+{
+	struct handle_fixture *f = data;
 	(void)params;
-
-	err = dqlite_config(server, DQLITE_CONFIG_LOGGER, logger);
-	munit_assert_int(err, ==, 0);
-
-	munit_assert_ptr_equal(dqlite_logger(server), logger);
-
-	free(logger);
-
+	HANDLE(f->sockets.client);
+	f->sockets.server_disconnected = true;
 	return MUNIT_OK;
 }
-
-TEST_CASE(config, heartbeat_timeout, NULL)
-{
-	dqlite *server  = data;
-	int            timeout = 1000;
-	int            err;
-
-	(void)params;
-
-	err =
-	    dqlite_config(server, DQLITE_CONFIG_HEARTBEAT_TIMEOUT, &timeout);
-	munit_assert_int(err, ==, 0);
-
-	return MUNIT_OK;
-}
-
-TEST_CASE(config, page_size, NULL)
-{
-	dqlite *server = data;
-	int            size   = 512;
-	int            err;
-
-	(void)params;
-
-	err = dqlite_config(server, DQLITE_CONFIG_PAGE_SIZE, &size);
-	munit_assert_int(err, ==, 0);
-
-	return MUNIT_OK;
-}
-
-TEST_CASE(config, checkpoint_threshold, NULL)
-{
-	dqlite *server    = data;
-	int            threshold = 1;
-	int            err;
-
-	(void)params;
-
-	err = dqlite_config(
-	    server, DQLITE_CONFIG_CHECKPOINT_THRESHOLD, &threshold);
-	munit_assert_int(err, ==, 0);
-
-	return MUNIT_OK;
-}
-
-#endif
