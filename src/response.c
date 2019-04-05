33e3d631f512b92314dd1d7558948dfd6513608b
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-04-05 21:57:43 +0200

    Drop old response objects

--- src/response.c
+++ src/response.c
@@ -1,31 +1,6 @@
-#include <stdlib.h>
-#include <string.h>
-
-#include <sqlite3.h>
-
-#include "../include/dqlite.h"
-
-#include "./lib/assert.h"
-
-#include "error.h"
-#include "lifecycle.h"
-#include "message.h"
 #include "response.h"
-#include "schema.h"
 
 #define RESPONSE__IMPLEMENT(LOWER, UPPER, _) \
 	SERIALIZE__IMPLEMENT(response_##LOWER, RESPONSE_##UPPER);
 
 RESPONSE__TYPES(RESPONSE__IMPLEMENT, );
-
-SCHEMA__IMPLEMENT(response_failure_, RESPONSE__SCHEMA_FAILURE);
-SCHEMA__IMPLEMENT(response_server_, RESPONSE__SCHEMA_SERVER);
-SCHEMA__IMPLEMENT(response_welcome_, RESPONSE__SCHEMA_WELCOME);
-SCHEMA__IMPLEMENT(response_servers, RESPONSE__SCHEMA_SERVERS);
-SCHEMA__IMPLEMENT(response_db_, RESPONSE__SCHEMA_DB);
-SCHEMA__IMPLEMENT(response_stmt_, RESPONSE__SCHEMA_STMT);
-SCHEMA__IMPLEMENT(response_result_, RESPONSE__SCHEMA_RESULT);
-SCHEMA__IMPLEMENT(response_rows_, RESPONSE__SCHEMA_ROWS);
-SCHEMA__IMPLEMENT(response_empty_, RESPONSE__SCHEMA_EMPTY);
-
-SCHEMA__HANDLER_IMPLEMENT(response, RESPONSE__SCHEMA_TYPES);
