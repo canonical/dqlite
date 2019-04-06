f35bd9e836283f1cb7b35f53ef608a60c5a2fcbf
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-04-06 10:57:14 +0200

    Implement connect function in proxy transport

--- src/transport.h
+++ src/transport.h
@@ -7,6 +7,8 @@
 
 #include <raft/io_uv.h>
 
+#include "../include/dqlite.h"
+
 int raft_uv_proxy__init(struct raft_io_uv_transport *transport,
 			struct uv_loop_s *loop);
 
@@ -19,5 +21,11 @@ void raft_uv_proxy__accept(struct raft_io_uv_transport *transport,
 			   unsigned id,
 			   const char *address,
 			   struct uv_stream_s *stream);
+
+/**
+ * Set a custom connect function.
+ */
+void raft_uv_proxy__set_connect_func(struct raft_io_uv_transport *transport,
+				     dqlite_connect connect);
 
 #endif /* TRANSPORT_H_*/
