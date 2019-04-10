9e94e68ed1a14736810b30029d4314b74a11b4f0
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-04-10 13:47:26 +0200

    Add dry_run field to struct tx

--- src/tx.h
+++ src/tx.h
@@ -6,17 +6,18 @@
 #include <sqlite3.h>
 
 enum { TX__PENDING = 0, /* Initial state right after creation */
-       TX__WRITING, /* After a non-commit frames command has been applied. */
-       TX__WRITTEN, /* After a commit frames command has been applied. */
-       TX__UNDONE,  /* After an undo command has been executed. */
-       TX__DOOMED   /* The transaction has errored. */
+       TX__WRITING,     /* After a non-commit frames command was applied. */
+       TX__WRITTEN,     /* After a commit frames command was applied. */
+       TX__UNDONE,      /* After an undo command has been executed. */
+       TX__DOOMED       /* The transaction has errored. */
 };
 struct tx
 {
-	unsigned long long id;
-	sqlite3 *conn;
-	bool is_zombie;
-	int state;
+	unsigned long long id; /* Transaction ID. */
+	sqlite3 *conn;         /* Underlying SQLite connection */
+	bool is_zombie;        /* Whether this is a zombie transaction */
+	bool dry_run;          /* Don't invoke actual SQLite hooks. */
+	int state;             /* Current state */
 };
 
 void tx__init(struct tx *tx, unsigned long long id, sqlite3 *conn);
