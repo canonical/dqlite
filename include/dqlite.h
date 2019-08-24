69bbd289f40591d138a5889e94daeb24f2b09b45
Free Ekanayaka <free.ekanayaka@canonical.com>
2019-08-24 11:22:12 +0200

    Drop dqlite_cluster

--- include/dqlite.h
+++ include/dqlite.h
@@ -75,13 +75,6 @@ enum { DQLITE_UNAVAILABLE, DQLITE_FOLLOWER, DQLITE_CANDIDATE, DQLITE_LEADER };
 /* Special value indicating that the result set is complete. */
 #define DQLITE_RESPONSE_ROWS_DONE 0xffffffffffffffff
 
-/* Information about a single dqlite server. */
-struct dqlite_server
-{
-	unsigned id;
-	const char *address;
-};
-
 /* Handle connections from dqlite clients */
 typedef struct dqlite_task dqlite_task;
 
@@ -111,14 +104,6 @@ typedef void (*dqlite_watch)(void *data, int old_state, int new_state);
  * dqlite_run.
  */
 int dqlite_config(dqlite_task *t, int op, ...);
-
-/* Return information about all servers currently part of the dqlite cluster.
- *
- * In case of success, the caller is responsible for freeing the returned array
- * using sqlite3_free().  */
-int dqlite_cluster(dqlite_task *t,
-		   struct dqlite_server *servers[],
-		   unsigned *n);
 
 /* Stop a dqlite server.
 **
