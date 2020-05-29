canonical/dqlite

commit 30fd9c57077e75386db6557f5201adb99b72d76a

Check if the node is the leader also when serving SQL exec requests
Some SQL exec requests such as BEGIN don't actually trigger any raft command, so
we need to explicitly check for leadership when starting to serve the request.

src/gateway.c
@@ -245,6 +245,7 @@ static int handle_exec(struct handle *req, struct cursor *cursor)
 	struct stmt *stmt;
 	int rv;
 	START(exec, result);
+	CHECK_LEADER(req);
 	LOOKUP_DB(request.db_id);
 	LOOKUP_STMT(request.stmt_id);
 	(void)response;
@@ -450,6 +451,7 @@ static int handle_exec_sql(struct handle *req, struct cursor *cursor)
 {
 	struct gateway *g = req->gateway;
 	START(exec_sql, result);
+	CHECK_LEADER(req);
 	LOOKUP_DB(request.db_id);
 	(void)response;
 	assert(g->req == NULL);

commit b2ff79bfb65660a0864937d50818d40ee8d4d1c0

Fix error message assertions which regressed after #226

src/gateway.c
@@ -451,7 +451,6 @@ static int handle_exec_sql(struct handle *req, struct cursor *cursor)
 {
 	struct gateway *g = req->gateway;
 	START(exec_sql, result);
-	CHECK_LEADER(req);
 	LOOKUP_DB(request.db_id);
 	(void)response;
 	assert(g->req == NULL);

test/unit/test_gateway.c
@@ -603,7 +603,7 @@ TEST_CASE(exec, frames_not_leader_1st_non_commit_re_elected, NULL)
 	CLUSTER_DEPOSE;
 	EXEC_SUBMIT(stmt_id);
 	ASSERT_CALLBACK(0, FAILURE);
-	ASSERT_FAILURE(SQLITE_IOERR_NOT_LEADER, "disk I/O error");
+	ASSERT_FAILURE(SQLITE_IOERR_NOT_LEADER, "not leader");
 
 	/* Re-elect ourselves and re-try */
 	CLUSTER_ELECT(0);
@@ -636,7 +636,7 @@ TEST_CASE(exec, frames_not_leader_1st_non_commit_other_elected, NULL)
 	CLUSTER_DEPOSE;
 	EXEC_SUBMIT(stmt_id);
 	ASSERT_CALLBACK(0, FAILURE);
-	ASSERT_FAILURE(SQLITE_IOERR_NOT_LEADER, "disk I/O error");
+	ASSERT_FAILURE(SQLITE_IOERR_NOT_LEADER, "not leader");
 
 	/* Elect another leader and re-try */
 	CLUSTER_ELECT(1);
@@ -672,7 +672,7 @@ TEST_CASE(exec, frames_not_leader_2nd_non_commit_re_elected, NULL)
 	CLUSTER_DEPOSE;
 	EXEC_SUBMIT(stmt_id);
 	ASSERT_CALLBACK(0, FAILURE);
-	ASSERT_FAILURE(SQLITE_IOERR_NOT_LEADER, "disk I/O error");
+	ASSERT_FAILURE(SQLITE_IOERR_NOT_LEADER, "not leader");
 
 	/* Re-elect ourselves and re-try */
 	CLUSTER_ELECT(0);
@@ -730,7 +730,6 @@ TEST_CASE(exec, frames_not_leader_2nd_non_commit_other_elected, NULL)
 	CLUSTER_DEPOSE;
 	EXEC_SUBMIT(stmt_id);
 	ASSERT_CALLBACK(0, FAILURE);
-	ASSERT_FAILURE(SQLITE_IOERR_NOT_LEADER, "disk I/O error");
 
 	/* Elect another leader and re-try */
 	CLUSTER_ELECT(1);
@@ -831,7 +830,8 @@ TEST_CASE(exec, undo_not_leader_pending_re_elected, NULL)
 	PREPARE("ROLLBACK");
 	CLUSTER_DEPOSE;
 	EXEC_SUBMIT(stmt_id);
-	ASSERT_CALLBACK(0, RESULT);
+	ASSERT_CALLBACK(0, FAILURE);
+	ASSERT_FAILURE(SQLITE_IOERR_NOT_LEADER, "not leader");
 
 	/* Re-elect ourselves and re-try */
 	CLUSTER_ELECT(0);
@@ -862,7 +862,8 @@ TEST_CASE(exec, undo_not_leader_pending_other_elected, NULL)
 	PREPARE("ROLLBACK");
 	CLUSTER_DEPOSE;
 	EXEC_SUBMIT(stmt_id);
-	ASSERT_CALLBACK(0, RESULT);
+	ASSERT_CALLBACK(0, FAILURE);
+	ASSERT_FAILURE(SQLITE_IOERR_NOT_LEADER, "not leader");
 
 	/* Re-elect ourselves and re-try */
 	CLUSTER_ELECT(1);