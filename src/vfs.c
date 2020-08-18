caa1cb32eb0b79c3206f6d9d8cf46a149043d44f
Free Ekanayaka <free.ekanayaka@canonical.com>
2020-08-18 16:12:57 +0200

    vfs: Initialize WAL header in VfsApply instead of sniffing journal_mode

--- src/vfs.c
+++ src/vfs.c
@@ -21,6 +21,18 @@
 /* Maximum pathname length supported by this VFS. */
 #define VFS__MAX_PATHNAME 512
 
+/* WAL magic value. Either this value, or the same value with the least
+ * significant bit also set (FORMAT__WAL_MAGIC | 0x00000001) is stored in 32-bit
+ * big-endian format in the first 4 bytes of a WAL file.
+ *
+ * If the LSB is set, then the checksums for each frame within the WAL file are
+ * calculated by treating all data as an array of 32-bit big-endian
+ * words. Otherwise, they are calculated by interpreting all data as 32-bit
+ * little-endian words. */
+#define VFS__WAL_MAGIC 0x377f0682
+
+#define VFS__WAL_MAX_VERSION 3007000
+
 /* Hold content for a shared memory mapping. */
 struct vfsShm
 {
@@ -90,6 +102,52 @@ static uint32_t vfsFlip32(uint32_t v)
 static uint32_t vfsGet32(const uint8_t buf[4])
 {
 	return vfsFlip32(*(const uint32_t *)buf);
+}
+
+/* Store a 32-bit number in big endian format */
+static void vfsPut32(uint32_t v, uint8_t *buf)
+{
+	uint32_t u = vfsFlip32(v);
+	memcpy(buf, &u, sizeof u);
+}
+
+/*
+ * Generate or extend an 8 byte checksum based on the data in array data[] and
+ * the initial values of in[0] and in[1] (or initial values of 0 and 0 if
+ * in==NULL).
+ *
+ * The checksum is written back into out[] before returning.
+ *
+ * n must be a positive multiple of 8. */
+static void vfsChecksum(
+    uint8_t *data, /* Content to be checksummed */
+    unsigned n,    /* Bytes of content in a[].  Must be a multiple of 8. */
+    const uint32_t in[2], /* Initial checksum value input */
+    uint32_t out[2]       /* OUT: Final checksum value output */
+)
+{
+	uint32_t s1, s2;
+	uint32_t *cur = (uint32_t *)data;
+	uint32_t *end = (uint32_t *)&data[n];
+
+	if (in) {
+		s1 = in[0];
+		s2 = in[1];
+	} else {
+		s1 = s2 = 0;
+	}
+
+	assert(n >= 8);
+	assert((n & 0x00000007) == 0);
+	assert(n <= 65536);
+
+	do {
+		s1 += *cur++ + s2;
+		s2 += *cur++ + s1;
+	} while (cur < end);
+
+	out[0] = s1;
+	out[1] = s2;
 }
 
 /* Create a new frame of a WAL file. */
@@ -913,6 +971,21 @@ static int vfsFileRead(sqlite3_file *file,
 	return rv;
 }
 
+static uint32_t vfsDatabaseGetPageSize(struct vfsDatabase *d)
+{
+	uint8_t *page;
+
+	assert(d->n_pages > 0);
+
+	page = d->pages[0];
+
+	/* The page size is stored in the 16th and 17th bytes of the first
+	 * database page (big-endian) */
+	uint8_t buf[4] = {0, 0, page[16], page[17]};
+
+	return vfsDecodePageSize(buf);
+}
+
 static int vfsDatabaseWrite(struct vfsDatabase *d,
 			    const void *buf,
 			    int amount,
@@ -1224,10 +1297,6 @@ static int vfsFileControlPragma(struct vfsFile *f, char **fnctl)
 		if (strcasecmp(right, "wal") != 0) {
 			fnctl[0] = "only WAL mode is supported";
 			return SQLITE_IOERR;
-		}
-		if (f->database->wal.n_frames == 0) {
-			formatWalInitHeader(f->database->wal.hdr,
-					    f->database->page_size);
 		}
 	}
 
@@ -2124,6 +2193,21 @@ oom:
 	return DQLITE_NOMEM;
 }
 
+/* Write the header of a brand new WAL file image. */
+static void vfsWalStartHeader(struct vfsWal *w, uint32_t page_size)
+{
+	assert(page_size > 0);
+	uint32_t checksum[2] = {0, 0};
+	vfsPut32(VFS__WAL_MAGIC, &w->hdr[0]);
+	vfsPut32(VFS__WAL_MAX_VERSION, &w->hdr[4]);
+	vfsPut32(page_size, &w->hdr[8]);
+	vfsPut32(0, &w->hdr[12]);
+	sqlite3_randomness(8, &w->hdr[16]);
+	vfsChecksum(w->hdr, 24, checksum, checksum);
+	vfsPut32(checksum[0], w->hdr + 24);
+	vfsPut32(checksum[1], w->hdr + 28);
+}
+
 int VfsApply(sqlite3_vfs *vfs,
 	     const char *filename,
 	     unsigned n,
@@ -2143,6 +2227,13 @@ int VfsApply(sqlite3_vfs *vfs,
 
 	wal = &database->wal;
 	shm = &database->shm;
+
+	/* If there's no page size set in the WAL header, it must mean that WAL
+	 * file was never written. In that case we need to initialize the WAL
+	 * header. */
+	if (vfsWalGetPageSize(wal) == 0) {
+		vfsWalStartHeader(wal, vfsDatabaseGetPageSize(database));
+	}
 
 	rv = vfsWalAppend(wal, database->n_pages, n, page_numbers, frames);
 	if (rv != 0) {
