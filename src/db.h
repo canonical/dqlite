970c745e907a01900ed78fb6e1fad6fc69a49f9b
Free Ekanayaka <free.ekanayaka@canonical.com>
2020-08-26 12:55:44 +0200

    db: Drop unused db->opening field

--- src/db.h
+++ src/db.h
@@ -13,7 +13,6 @@ struct db
 {
 	struct config *config; /* Dqlite configuration */
 	char *filename;        /* Database filename */
-	bool opening;          /* Whether an Open request is in progress */
 	sqlite3 *follower;     /* Follower connection */
 	queue leaders;         /* Open leader connections */
 	unsigned tx_id;        /* Current ongoing transaction ID, if any */
