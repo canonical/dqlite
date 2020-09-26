785fb3cff0c5b6bf195606c3444e4b5551325045
Free Ekanayaka <free.ekanayaka@canonical.com>
2020-09-26 13:13:14 +0200

    Don't close client connections when stepping down from leader

--- src/server.c
+++ src/server.c
@@ -465,13 +465,19 @@ static void monitor_cb(uv_prepare_t *monitor)
 {
 	struct dqlite_node *d = monitor->data;
 	int state = raft_state(&d->raft);
+	/*
 	queue *head;
 	struct conn *conn;
+	*/
 
 	if (state == RAFT_UNAVAILABLE) {
 		return;
 	}
 
+	/* TODO: we should shutdown clients that are performing SQL requests,
+	 * but not the ones which are doing management-requests, such as
+	 * transfer leadership.  */
+	/*
 	if (d->raft_state == RAFT_LEADER && state != RAFT_LEADER) {
 		QUEUE__FOREACH(head, &d->conns)
 		{
@@ -479,6 +485,7 @@ static void monitor_cb(uv_prepare_t *monitor)
 			conn__stop(conn);
 		}
 	}
+	*/
 
 	d->raft_state = state;
 }
