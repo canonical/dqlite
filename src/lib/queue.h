8d4ec3ecac6f7f405b51c7e31e0099dfc360c60d
Eric Curtin <ericcurtin17@gmail.com>
2020-11-23 22:53:25 +0000

    s/QUEUE__NEXT/QUEUE_NEXT/g

--- src/lib/queue.h
+++ src/lib/queue.h
@@ -6,32 +6,32 @@
 typedef void *queue[2];
 
 /* Private macros. */
-#define QUEUE__NEXT(q) (*(queue **)&((*(q))[0]))
+#define QUEUE_NEXT(q) (*(queue **)&((*(q))[0]))
 #define QUEUE__PREV(q) (*(queue **)&((*(q))[1]))
 
-#define QUEUE__PREV_NEXT(q) (QUEUE__NEXT(QUEUE__PREV(q)))
-#define QUEUE__NEXT_PREV(q) (QUEUE__PREV(QUEUE__NEXT(q)))
+#define QUEUE__PREV_NEXT(q) (QUEUE_NEXT(QUEUE__PREV(q)))
+#define QUEUE_NEXT_PREV(q) (QUEUE__PREV(QUEUE_NEXT(q)))
 
 /**
  * Initialize an empty queue.
  */
 #define QUEUE_INIT(q)                 \
 	{                             \
-		QUEUE__NEXT(q) = (q); \
+		QUEUE_NEXT(q) = (q);  \
 		QUEUE__PREV(q) = (q); \
 	}
 
 /**
  * Return true if the queue has no element.
  */
-#define QUEUE_IS_EMPTY(q) ((const queue *)(q) == (const queue *)QUEUE__NEXT(q))
+#define QUEUE_IS_EMPTY(q) ((const queue *)(q) == (const queue *)QUEUE_NEXT(q))
 
 /**
  * Insert an element at the back of a queue.
  */
 #define QUEUE__PUSH(q, e)                        \
 	{                                        \
-		QUEUE__NEXT(e) = (q);            \
+		QUEUE_NEXT(e) = (q);             \
 		QUEUE__PREV(e) = QUEUE__PREV(q); \
 		QUEUE__PREV_NEXT(e) = (e);       \
 		QUEUE__PREV(q) = (e);            \
@@ -41,16 +41,16 @@ typedef void *queue[2];
  * Remove the given element from the queue. Any element can be removed at any
  * time.
  */
-#define QUEUE__REMOVE(e)                              \
-	{                                             \
-		QUEUE__PREV_NEXT(e) = QUEUE__NEXT(e); \
-		QUEUE__NEXT_PREV(e) = QUEUE__PREV(e); \
+#define QUEUE__REMOVE(e)                             \
+	{                                            \
+		QUEUE__PREV_NEXT(e) = QUEUE_NEXT(e); \
+		QUEUE_NEXT_PREV(e) = QUEUE__PREV(e); \
 	}
 
 /**
  * Return the element at the front of the queue.
  */
-#define QUEUE_HEAD(q) (QUEUE__NEXT(q))
+#define QUEUE_HEAD(q) (QUEUE_NEXT(q))
 
 /**
  * Return the element at the back of the queue.
@@ -63,7 +63,7 @@ typedef void *queue[2];
  * Mutating the queue while iterating results in undefined behavior.
  */
 #define QUEUE_FOREACH(q, e) \
-	for ((q) = QUEUE__NEXT(e); (q) != (e); (q) = QUEUE__NEXT(q))
+	for ((q) = QUEUE_NEXT(e); (q) != (e); (q) = QUEUE_NEXT(q))
 
 /**
  * Return the structure holding the given element.
