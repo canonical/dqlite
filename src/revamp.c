f638fd733c48f48019f5dff34716d4f441c51218
Cole Miller <cole.miller@canonical.com>
2023-10-18 00:01:57 -0400

    WIP: start working on a way to send EXEC_SQL to the DB thread
    This has a deadlock bug that needs to be ironed out.
    
    Signed-off-by: Cole Miller <cole.miller@canonical.com>

--- src/revamp.c
+++ src/revamp.c
@@ -3,16 +3,47 @@
 int dbContextInit(struct db_context *ctx, struct config *config)
 {
 	int rv;
-	rv = sem_init(&ctx->sem, 0, 0);
-	if (rv != 0) {
-		return DQLITE_ERROR;
-	}
+	rv = pthread_mutex_init(&ctx->mutex, NULL);
+	assert(rv == 0);
+	rv = pthread_cond_init(&ctx->cond, NULL);
+	assert(rv == 0);
 	registry__init(&ctx->registry, config);
+	ctx->shutdown = false;
+	return 0;
+}
+
+int postExecSqlReq(struct db_context *ctx,
+		struct exec_sql_req req,
+		void *data,
+		void (*cb)(struct exec_sql_req, int, void *))
+{
+	(void)ctx;
+	(void)req;
+	(void)data;
+	(void)cb;
 	return 0;
 }
 
 void dbContextClose(struct db_context *ctx)
 {
 	registry__close(&ctx->registry);
-	sem_destroy(&ctx->sem);
+	pthread_cond_destroy(&ctx->cond);
+	pthread_mutex_destroy(&ctx->mutex);
+}
+
+void *dbTask(void *arg)
+{
+	struct db_context *ctx = arg;
+	int rv;
+
+	rv = pthread_mutex_lock(&ctx->mutex);
+	assert(rv == 0);
+	for (;;) {
+		rv = pthread_cond_wait(&ctx->cond, &ctx->mutex);
+		assert(rv == 0);
+		if (ctx->shutdown) {
+			break;
+		}
+	}
+	return NULL;
 }
