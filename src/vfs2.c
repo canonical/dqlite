0a7cc1266154f4deaf0d5fc2b09b4f77dc48e701
Cole Miller <cole.miller@canonical.com>
2024-07-17 15:31:09 -0400

    Initialize page size in vfs2_add_uncommitted if not earlier
    A follower that first touches a database when it receives a
    COMMAND_FRAMES entry needs to initialize the page size at that point
    (absent a guarantee that we run `PRAGMA page_size` immediately after
    sqlite3_open).
    
    Signed-off-by: Cole Miller <cole.miller@canonical.com>

--- src/vfs2.c
+++ src/vfs2.c
@@ -1710,6 +1710,9 @@ int vfs2_add_uncommitted(sqlite3_file *file,
 	struct file *xfile = (struct file *)file;
 	PRE(xfile->flags & SQLITE_OPEN_MAIN_DB);
 	struct entry *e = xfile->entry;
+	if (e->page_size == 0) {
+		e->page_size = page_size;
+	}
 	PRE(page_size == e->page_size);
 	int rv;
 
