60fda14cae34ab785d6c54e01d07e65f9ca5a9aa
Marco Manino <mrcmnn@gmail.com>
2025-05-23 17:46:03 +0200

    revert whitespace change

--- src/bind.c
+++ src/bind.c
@@ -9,35 +9,35 @@ static int bind_one(sqlite3_stmt *stmt, int n, struct value *value)
 	int rc;
 
 	switch (value->type) {
-		case SQLITE_INTEGER:
-			rc = sqlite3_bind_int64(stmt, n, value->integer);
-			break;
-		case SQLITE_FLOAT:
-			rc = sqlite3_bind_double(stmt, n, value->real);
-			break;
-		case SQLITE_BLOB:
-			rc = sqlite3_bind_blob(stmt, n, value->blob.base,
-					       (int)value->blob.len,
-					       SQLITE_STATIC);
-			break;
-		case SQLITE_NULL:
-			rc = sqlite3_bind_null(stmt, n);
-			break;
-		case SQLITE_TEXT:
-			rc = sqlite3_bind_text(stmt, n, value->text, -1,
-					       SQLITE_STATIC);
-			break;
-		case DQLITE_ISO8601:
-			rc = sqlite3_bind_text(stmt, n, value->text, -1,
-					       SQLITE_STATIC);
-			break;
-		case DQLITE_BOOLEAN:
-			rc = sqlite3_bind_int64(stmt, n,
-						value->boolean == 0 ? 0 : 1);
-			break;
-		default:
-			rc = SQLITE_ERROR;
-			break;
+	case SQLITE_INTEGER:
+		rc = sqlite3_bind_int64(stmt, n, value->integer);
+		break;
+	case SQLITE_FLOAT:
+		rc = sqlite3_bind_double(stmt, n, value->real);
+		break;
+	case SQLITE_BLOB:
+		rc = sqlite3_bind_blob(stmt, n, value->blob.base,
+						(int)value->blob.len,
+						SQLITE_STATIC);
+		break;
+	case SQLITE_NULL:
+		rc = sqlite3_bind_null(stmt, n);
+		break;
+	case SQLITE_TEXT:
+		rc = sqlite3_bind_text(stmt, n, value->text, -1,
+						SQLITE_STATIC);
+		break;
+	case DQLITE_ISO8601:
+		rc = sqlite3_bind_text(stmt, n, value->text, -1,
+						SQLITE_STATIC);
+		break;
+	case DQLITE_BOOLEAN:
+		rc = sqlite3_bind_int64(stmt, n,
+					value->boolean == 0 ? 0 : 1);
+		break;
+	default:
+		rc = SQLITE_ERROR;
+		break;
 	}
 
 	if (rc != 0) {
