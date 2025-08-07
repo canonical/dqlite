263c8b2e781484aff1e567c9e7a0f3498fecd0dd
Marco Manino <mrcmnn@gmail.com>
2025-08-07 09:35:18 +0200

    chore: embed human-readable version string in binary

--- src/dqlite.c
+++ src/dqlite.c
@@ -4,3 +4,17 @@ int dqlite_version_number(void)
 {
 	return DQLITE_VERSION_NUMBER;
 }
+
+#define STRINGIFY_HELPER(x) #x
+#define STRINGIFY(x) STRINGIFY_HELPER(x)
+
+const char *dqlite_version_string =
+	STRINGIFY(DQLITE_VERSION_MAJOR) "."
+	STRINGIFY(DQLITE_VERSION_MINOR) "."
+	STRINGIFY(DQLITE_VERSION_RELEASE) "@"
+#ifdef DQLITE_VERSION_BUILD_TAG
+ 	DQLITE_VERSION_BUILD_TAG
+#else
+	"unknown"
+#endif
+;
